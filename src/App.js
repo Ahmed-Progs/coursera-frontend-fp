49876b0473d369f8db290de18335ab4dcb8f3f65
Ahmed Raza <145737642+Ahmed-Progs@users.noreply.github.com>
2025-09-24 23:25:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./sections/landingPage";         // Hero, About, Specials sections included here
-import BookingPage from "./reservaTable/bookingPage"; // wrapper for BookingForm
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./sections/landingPage";
+import BookingPage from "./reservaTable/bookingPage";
 import ConfirmedBooking from "./reservaTable/confirmedBooking";
 import Navbar from "./sections/header";
 import Footer from "./sections/footer";
@@ -8,13 +8,13 @@ import Footer from "./sections/footer";
 function App() {
   return (
     <Router>
-      <Navbar /> {/* Navbar visible on all pages */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/reserve" element={<BookingPage />} />
         <Route path="/confirmed" element={<ConfirmedBooking />} />
       </Routes>
-      <Footer /> {/* Footer visible on all pages */}
+      <Footer />
     </Router>
   );
 }
